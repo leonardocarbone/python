f65721980c36ec173f3c9bb791681b82e3339326
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-17 13:15:09 -0300

    Update classes.py

--- examples/classes.py
+++ examples/classes.py
@@ -1,5 +1,6 @@
 # Example 1 - Super Simple Class
 class Animal(object):
+  
   def __init__(self):
     print "Animal Created!"
     
@@ -12,8 +13,7 @@ class Animal(object):
 a = Animal()
 a.eat()    
     
-# Example 2 - Inheritance    
-
+# Example 2 - Inheritence    
 class Dog(Animal):
   
   def __init__(self):
@@ -25,8 +25,39 @@ class Dog(Animal):
     
   def bark(self):
     print "woof!"
-
+    
 d = Dog()
 d.whoAmI()
 d.eat()
 d.bark()
+
+# Example - Special methods
+class Book(object):
+  
+  # Initializer, constructor
+  def __init__(self, title, author, pages):
+    print "A book has been created!"
+    self.title = title
+    self.author = author
+    self.pages = pages
+    
+  
+  # Equivalent to toString on java. Used with built-in function 'print'
+  def __str__(self):
+    return "Title: {0} - Author: {1} - Pages: {2}".format(self.title, self.author, self.pages)
+    
+  # Used in conjunction with built-in function 'len'
+  def __len__(self):
+    return self.pages
+    
+  # Used in conjunction with built-in function 'del'
+  def __del__(self):
+    print "Book Deleted!"
+    
+b = Book("Python for Dummies","Someone",1254)
+print(b)
+print len(b)
+
+del b
+b.title # NameError. B is not defined
+  
