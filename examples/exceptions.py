7545b2fcb12596ae105018897d354a495dc64cd1
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-17 22:58:54 -0300

    Update exceptions.py

--- examples/exceptions.py
+++ examples/exceptions.py
@@ -1,6 +1,13 @@
-try:
-  2/0
-except Exception, e:
-  print 'Error - ' + str(e)
-finally:
-  print 'Done!'
+def division(value1, value2):
+  try:
+    value1 / value2
+  except Exception, e:
+    print 'On error - ' + str(e)
+  else:
+    print "On else!"
+  finally:
+    print 'On finally!'
+    
+division(2,2)
+print ""
+division(2,0)
