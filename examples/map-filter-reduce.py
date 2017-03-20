1e68051828839fdbe7b1b894bc8e3f8fa54c8347
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-20 13:33:18 -0300

    Create map-filter-reduce.py

---
+++ examples/map-filter-reduce.py
@@ -0,0 +1,19 @@
+# Example 1 - Use map to convert a list of string numbers in a list of integers
+a = ["1","2","3"]
+print a
+a = map(int, a)
+print a
+
+# Example 2 - Use map to apply some function to a value
+def hello(name):
+  print "Hello", name
+  
+names = ["Leo","Jon","Arthur"]
+map(hello, names)
+
+# Example 3 - Use map to combine two lists
+def say_my_name(name, surname):
+  print "Say my name -> {0} {1}".format(name,surname)
+  
+surnames = ["Carbone","Hurht","Max"]
+map(lambda x,y: say_my_name(x,y), names, surnames)
