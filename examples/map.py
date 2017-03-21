359440037b5cf74ab6a2d1f1ca84026c87f5b896
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-21 13:29:11 -0300

    Update and rename map-filter-reduce.py to map.py

--- examples/map.py
+++ examples/map.py
@@ -17,12 +17,3 @@ def say_my_name(name, surname):
   
 surnames = ["Carbone","Hurht","Max"]
 map(lambda x,y: say_my_name(x,y), names, surnames)
-
-# Example 4 - Use reduce to find  max and min values from a list
-# Note. You can use built-in functions max e min to find these values
-min_value = lambda v1,v2: v1 if (v1<v2) else v2
-max_value = lambda v1,v2: v1 if (v1>v2) else v2
-
-list = [1,3,4,3,6,2,10]
-print reduce(min_value, list)
-print reduce(max_value, list)
