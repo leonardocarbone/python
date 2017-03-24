77af485b25480f23d77778d187389f44b9a91187
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-24 14:00:07 -0300

    Update enumerate.py

--- examples/enumerate.py
+++ examples/enumerate.py
@@ -12,3 +12,10 @@ def to_dict(list):
   return {key:value for value, key in enumerate(list)}
 
 print to_dict(["a" ,"b", "c"])  
+
+# Example 3 - Use enumerate and other functions to return the count of the number of items in the list
+# whose  value equals its index
+def count_match_index(list):
+  return len(filter(lambda tuple: tuple[0] == tuple[1], enumerate(list)))
+
+print count_match_index([0,2,2,1,5,5,6,10])
