4e5ad94ff559669b9bbd8e70db027c4d0cf59025
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-24 13:58:01 -0300

    Update reduce.py

--- examples/reduce.py
+++ examples/reduce.py
@@ -6,3 +6,9 @@ max_value = lambda v1,v2: v1 if (v1>v2) else v2
 list = [1,3,4,3,6,2,10]
 print reduce(min_value, list)
 print reduce(max_value, list)
+
+# Example 2 - Use reduce to take a list of digits and return the number that they correspond to
+def digits_to_num(digits):
+  return reduce(lambda x,y:x*10 +y, digits )
+
+print digits_to_num([1,2,3])  
