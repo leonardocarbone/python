ef344a9bcd9c02cfa5e28901df12a4924d4f14f5
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-24 13:59:46 -0300

    Update enumerate.py

--- examples/enumerate.py
+++ examples/enumerate.py
@@ -4,3 +4,11 @@ print list
 
 for index, value in enumerate(list):
   print "Index {0}, Value {1}".format(index, value)
+
+  
+# Example 2 - Use enumerate to return a dictionary which has the values of list as keys and the index
+# as the value.
+def to_dict(list):
+  return {key:value for value, key in enumerate(list)}
+
+print to_dict(["a" ,"b", "c"])  
