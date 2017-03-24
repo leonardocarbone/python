2593a468022a7fe56497fa10836a9bdb9924c838
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-24 13:57:08 -0300

    Update map.py

--- examples/map.py
+++ examples/map.py
@@ -17,3 +17,9 @@ def say_my_name(name, surname):
   
 surnames = ["Carbone","Hurht","Max"]
 map(lambda x,y: say_my_name(x,y), names, surnames)
+
+# Example 3 - Use map to create a function which finds the length of each word in the phrase
+def word_lengths(phrase):
+  return map(lambda x: len(x), phrase.split(" "))
+  
+print word_lengths("How long are the words in this phrase")
