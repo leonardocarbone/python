b9804e45b6b2e0f68115019444d4d694d8b27f5d
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-24 13:58:40 -0300

    Update filter.py

--- examples/filter.py
+++ examples/filter.py
@@ -1,11 +1,11 @@
-# Solution 1 - Filter even/odd numbers on the list using lambda expression
+# Example 1 - Filter even/odd numbers on the list using lambda expression
 
 list = range(6)
 print "Even numbers", filter(lambda n: n%2==0, list)
 print "Odd numbers", filter(lambda n: n%2!=0, list)
 
 
-# Solution 2 - Filter even/odd numbers on the list using classic method
+# Example 2 - Filter even/odd numbers on the list using classic method
 
 def check_even(number):
   if number%2 == 0:
@@ -22,3 +22,11 @@ def check_odd(number):
 list = range(22)
 print "Even numbers", filter(check_even, list)
 print "Odd numbers", filter(check_odd, list)
+
+
+# Example 3 - Use filter to return the words form a list of words which start with a target letter
+def filter_words(word_list, letter):
+  return filter(lambda x: x[0] == letter, word_list)
+
+list = ["hello", "are", "cat", "dog", "ham", "hi", "go", "to", "heart"]
+print filter_words(list, "h")  
