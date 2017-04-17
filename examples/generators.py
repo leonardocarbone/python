862441804c33abc7ee81cfceec7cac0acc482f8c
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-04-17 12:58:26 -0300

    Update generators.py

--- examples/generators.py
+++ examples/generators.py
@@ -30,3 +30,25 @@ def gensquares(n):
     
 for x in gensquares(10):
   print x
+
+  
+ # Example 3 - Generator that generates the squares of numbers up to some number N.
+
+def gensquares(number):
+  for x in range(number):
+    yield x**2
+    
+for x in gensquares(10):
+  print x
+  
+# Example 4 - Generator that yields "n" random numbers between a low and high number (that are inputs)
+import random
+
+def rand_num(low, high, n):
+  for x in range(n):
+    yield random.randint(low, high)
+    
+for num in rand_num(1,10,12):
+  print num
+  
+  
