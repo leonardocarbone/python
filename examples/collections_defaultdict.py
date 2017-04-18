f3886630ab05c26f35aff5bfc423c68bfffd3881
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-04-18 13:12:35 -0300

    Update collections_defaultdict.py

--- examples/collections_defaultdict.py
+++ examples/collections_defaultdict.py
@@ -8,9 +8,9 @@ print d["key1"]
 d = defaultdict(object)
 d["key1"] = "Test"
 print d["key1"]
-print d["key2"]
+print d["key2"] # Will never raise KeyError
 
 d = defaultdict(lambda: "python is cool! but the key does not exist!")
 d["key1"] = "Test"
 print d["key1"]
-print d["key2"]
+print d["key2"] # Will never raise KeyError
