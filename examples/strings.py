20e00efdf9cb0a41f80dbb800eff148b4e0c8f21
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-04-24 22:05:16 -0300

    Create strings.py

---
+++ examples/strings.py
@@ -0,0 +1,18 @@
+s = 'hello world'
+print s.capitalize()
+print s.upper()
+print s.lower()
+print s.count('l')
+print s.find('w')
+
+print s.capitalize().center(30,'-')
+
+print 'Hi\t Leonardo'
+
+print "hello".isalnum()
+print "ola".isalpha()
+
+print "lower".islower()
+
+print "hello".endswith('o')
+print "hello"[-1] == 'o'
